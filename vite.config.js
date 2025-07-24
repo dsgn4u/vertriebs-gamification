486300fa74d5b3905b01e8109b7bbc9047e1f0f5
dsgn4u <91143012+dsgn4u@users.noreply.github.com>
2025-07-24 17:55:27 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  root: '.', // stellt sicher, dass Vite im Projektverzeichnis schaut
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html', // wichtig: relativer Pfad zur HTML-Datei
+    },
+  },
+});
